d4ae39d8e542ea50ccb7e54151eb40667d2088cf
ivshxvi <ivhsxvi@gmail.com>
2024-03-04 14:03:11 +0000

    Add tokenisation

---
+++ models/Token.js
@@ -0,0 +1,42 @@
+const { v4: uuidv4 } = require("uuid");
+
+const db = require("../database/connect");
+
+class Token {
+
+    constructor({ tokenid, userid, token }){
+        this.tokenid = tokenid;
+        this.userid = userid;
+        this.token = token;
+    }
+
+    static async create(userid) {
+        const token = uuidv4()
+        const response  = await db.query("INSERT INTO tokens (userid, token) VALUES ($1, $2) RETURNING tokenid;",
+        [userid, token])
+        const newId = response.rows[0].tokenid
+        const newToken = await Token.getOneById(newId)
+        return newToken
+    }
+
+    static async getOneById(id) {
+        const response = await db.query("SELECT * FROM token WHERE tokenid = $1", [id]);
+        if (response.rows.length != 1) {
+            throw new Error("Unable to locate token.");
+        } else {
+            return new Token(response.rows[0]);
+        }
+    }
+
+    static async getOneByToken(token) {
+        const response = await db.query("SELECT * FROM token WHERE token = $1", [token]);
+        if (response.rows.length != 1) {
+            throw new Error("Unable to locate token.");
+        } else {
+            return new Token(response.rows[0]);
+        }
+    }
+
+}
+
+module.exports = Token;
