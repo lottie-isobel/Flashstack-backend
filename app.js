eb94d6c52cd8fe0d6cfc7d515adb733e2b247ec4
Tom Wood <dexterbyte01@gmail.com>
2024-03-04 17:08:58 +0000

    test Flashcard model and controller

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-
-
 const express = require('express');
 const cors = require('cors');
-
+const logRoutes = require('./middleware/logger');
 
 const app = express();
 
+const flashcardRouter = require('./routes/flashcardRoutes');
+
+
 app.use(cors());
 app.use(express.json());
+app.use(logRoutes)
+app.use('/cards', flashcardRouter)
 
 
 app.get("/", (req, res) => {
     res.json({
-        name: "Flashstacks backend",
-        description: "Flashstacks is an educational app"
+        name: "Flashstacks Backend",
+        description: "This is the root of the Flashstacks API!"
     })
 })
 
