5d8981bf29dfb17a5095f2e64aed3eef9524241e
Tom Wood <dexterbyte01@gmail.com>
2024-03-05 14:58:54 +0000

    fix create card route

--- routes/flashcardRoutes.js
+++ routes/flashcardRoutes.js
@@ -5,7 +5,7 @@ const flashcardController = require('../controllers/flashcardController.js');
 const flashcardRouter = Router();
 
 flashcardRouter.get("/deck/:id", flashcardController.getAllByDeckId)
-flashcardRouter.post("/:id", flashcardController.create)
+flashcardRouter.post("/deck/:id", flashcardController.create)
 flashcardRouter.get("/:id", flashcardController.getOneByCardId)
 flashcardRouter.patch("/:id", flashcardController.update)
 flashcardRouter.delete("/:id", flashcardController.destroy)
